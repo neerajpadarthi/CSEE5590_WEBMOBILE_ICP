87350af4c563a46e03324d09904ca870509c135d
neerajpadarthi <42651065+neerajpadarthi@users.noreply.github.com>
2019-09-07 15:30:50 -0500

    Update script.js

--- Mobile ICP3/Source/SourceCode/rock_paper_scissors_game/script.js
+++ Mobile ICP3/Source/SourceCode/rock_paper_scissors_game/script.js
@@ -1,15 +1,18 @@
 $(document).ready(function () {
     $("#r").click(function () {
+        // Calling function when click events and erasing text
         var equation = document.getElementById("equation");
         equation.innerHTML="";
         check("r");
     });
     $("#p").click(function () {
+        // Calling function when click events and erasing text
         var equation = document.getElementById("equation");
         equation.innerHTML="";
         check("p");
     });
     $("#s").click(function () {
+        // Calling function when click events and erasing text
         var equation = document.getElementById("equation");
         equation.innerHTML="";
         check("s");
@@ -21,6 +24,7 @@ function check(c1) {
 
     var equation = document.getElementById("equation");
 
+    // Using Math function to get the system options
     var comp = Math.random();
     if (comp <= 0.30) {
         c2 = "r";
@@ -31,13 +35,14 @@ function check(c1) {
     }
 
     if (c1 === c2) {
-
+    // if it same it tie
         equation.innerHTML="Tie";
     }
 
     else if (c1 === "r") {
-
+        // When user selected rock
         if (c2 === "s") {
+            // when sys is Siccsor
             equation.innerHTML="Rock wins"+ "!!! " +"You Win";
         }
         else {
@@ -46,8 +51,9 @@ function check(c1) {
     }
 
     else if (c1 === "p") {
-
+        // WHne user is paper
         if (c2 === "r") {
+            // when sys is rock
             equation.innerHTML="Paper wins"  + "!!! " +  "You Win!";
         }
         else {
@@ -56,8 +62,9 @@ function check(c1) {
     }
 
     else if (c1 === "s") {
-
+// When user is Scissor
         if (c2 === "r") {
+            // When user is rock
             equation.innerHTML="Rock wins"  + "!!! " +  "You Lost";
         }
         else {
